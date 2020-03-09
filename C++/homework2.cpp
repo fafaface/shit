f206b218164f129383226a31718153b5214e6500
yuehan lian <870315703@qq.com>
2020-03-09 19:01:47 +0800

    lab2

--- C++/homework2.cpp
+++ C++/homework2.cpp
@@ -2,59 +2,22 @@
 //
 
 #include <iostream>
-#include <time.h>
 #include <stdio.h> 
-#include <set>
-#include<vector>
-#include <string>
-#include <dos.h> 
-#include <windows.h>
+
 using namespace std;
+void rotate(vector<int>& nums, int k) {
+	int len = nums.size();
+	int l;
+	len - k < 0 ? l = k - len : l=len - k;
+	for (int i = 0; i < l; i++)
+		nums.push_back(nums[i]);
+	for (int i = 0; i < l; i++)
+		nums.erase(nums.begin());
 
-#define MAX 101
-//问题表示
-int k;			//求解结果表示
-int a[MAX][MAX];	//存放比赛日程表（行列下标为0的元素不用）
 
-void Plan(int k)
-{
-	int i, j, n, t, temp;
-	n = 2;			//n从2^1=2开始
-	a[1][1] = 1; a[1][2] = 2; //求解2个选手比赛日程,得到左上角元素
-	a[2][1] = 2; a[2][2] = 1;
-	for (t = 1; t < k; t++)	//迭代处理2^2(t=1)…,2^k(t=k-1)个选手
-	{
-		temp = n;				//temp=2^t
-		n = n * 2; 				//n=2^(t+1)
-		for (i = temp + 1; i <= n; i++)		//填左下角元素
-			for (j = 1; j <= temp; j++)
-				a[i][j] = a[i - temp][j] + temp; //产生左下角元素
-		for (i = 1; i <= temp; i++)		//填右上角元素
-			for (j = temp + 1; j <= n; j++)
-				a[i][j] = a[i + temp][(j + temp) % n];
-		for (i = temp + 1; i <= n; i++)		//填右下角元素
-			for (j = temp + 1; j <= n; j++)
-				a[i][j] = a[i - temp][j - temp];
-	}
 }
-
 int main()
 {
-	cin >> k; 
-	LARGE_INTEGER c1;//开始计时
-	LARGE_INTEGER c2;//结束计时
-	LARGE_INTEGER frequency;//计时器频率
-	QueryPerformanceFrequency(&frequency);
-	double quadpart = (double)frequency.QuadPart;
-	QueryPerformanceCounter(&c1);
-	Plan(k);
-	QueryPerformanceCounter(&c2);
-	int len = pow(2, k);
-	for (int i = 1; i <= len; ++i) {
-		for (int j = 1; j <= len; ++j)
-			cout << a[i][j] << "  ";
-		cout << endl;
-	}
-	cout << "高精度计数器用时：" << (double)((c2.QuadPart - c1.QuadPart) * 1.0 / quadpart * 1.0) * 1000000 << endl;
+
 	return 0;
 }
---
+++ C++/lab2.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <time.h>
+#include <stdio.h> 
+#include <set>
+#include <string>
+#include<vector>
+#include <dos.h> 
+#include <windows.h>
+using namespace std;
+int BF(char s[], char t[]) {
+	int index = 0;//初始化变量
+	int i = 0, j = 0;
+	while ((s[i] != '\0') && (t[j] != '\0'))//当两个字符串下标都没有到字符串结束位置
+	{
+		if (s[i] == t[j]) { i++; j++; }//相等,下标都向后移动一位
+		else {
+			index++; i = index; j = 0;//不相等,原始字符串下标向后移动一位,查询字符串从头继续寻找
+		}
+	}
+	if (t[j] == '\0') return index + 1;//如果查找的字符串已经到末尾了,说明匹配成功
+	else return 0;//没有到末尾,说明原始字符串已经遍历完成,其中没有需要查找的字符串
+}
+
+void GetNext(char T[], int next[])
+{
+	int j = 0, k = -1;
+	next[0] = -1;
+	while (T[j] != '\0')              	//直到字符串末尾
+	{
+		if (k == -1) {               	//无相同子串
+			next[++j] = 0; k = 0;
+		}
+		else if (T[j] == T[k]) {        	//确定next[j+1]的值
+			k++;               		//相等子串长度加1
+			next[++j] = k;
+		}
+		else k = next[k];        	//取T[0]…T[j]的下一个相等子串的长度
+	}
+}
+int nex[1001];
+int KMP(char s[], char t[]) {
+	int i = 0, j = 0;
+	while ((s[i] != '\0') && (t[j] != '\0'))
+	{
+		if (s[i] == t[j]) { i++; j++; }
+		else j = nex[j];
+		if (j == -1) {
+			i++; j++;
+		}
+	}
+	if (t[j] == '\0') return i-strlen(t)+1;
+	else return 0;
+}
+
+int main()
+{
+	char input[1001];
+	char search[1001];
+	cout << "输入主串input ";
+	cin >> input;
+	search[0] = 'c'; search[1] = 'a'; search[2] = 'd'; search[3] = 'a'; search[4] = 'b'; search[5] = '\0';
+	//cout << "输入模式search: ";
+	//cin >> search;
+	memset(nex, 0, sizeof(nex));
+	GetNext(search, nex);
+	LARGE_INTEGER c1;//开始计时
+	LARGE_INTEGER c2;//结束计时
+	LARGE_INTEGER frequency;//计时器频率
+	QueryPerformanceFrequency(&frequency);
+	double quadpart = (double)frequency.QuadPart;
+	QueryPerformanceCounter(&c1);
+	int ans = KMP(input, search);//此处可替换为KMP,BM函数
+	QueryPerformanceCounter(&c2);
+	cout << "位置: " << ans << endl;
+	cout << "高精度计数器用时：" << (double)((c2.QuadPart - c1.QuadPart) * 1.0 / quadpart * 1.0) * 1000000000000 << endl;
+	return 0;
+}
+
