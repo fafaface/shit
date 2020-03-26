8b3e85f87b1d17fcf4f17e22a2dea93f3113e217
yuehan lian <870315703@qq.com>
2020-03-26 08:39:31 +0800

    算法设计实验3

--- C++/LeetCode.cpp
+++ C++/LeetCode.cpp
@@ -2,21 +2,39 @@
 #include <time.h>
 #include <stdio.h> 
 #include <set>
+#include<vector>
+#include<string>
 using namespace std;
-string convert(string s, int numRows) {
-	int len=s.length();
-	int str[9999][9999];
 
-
-
-	return s;
+vector<string> fizzBuzz(int n) {
+	string b1, b2, b3;
+	b1="Fizz"; b2="Buzz"; b3="FizzBuzz";
+	vector<string> ans;
+	for (int i = 1; i <= n; ++i)
+	{
+		if (i % 3 != 0 && i % 5 != 0)
+			ans.push_back(to_string(i));
+		else if (i % 3 == 0 && i % 5 != 0) {
+			ans.push_back(b1);
+		}
+		else if (i % 3 != 0 && i % 5 == 0) {
+			ans.push_back(b2);
+		}
+		else {
+			ans.push_back(b3);
+		}
+	}
+	return ans;
 }
-//int main()
+
+int main()
 {
-	string s;
-	int num;
-	cin >> s;
-	cin >> num;
-	s=convert(s, num);
+	vector<string> v1,v2;
+	int n,x;
+	cin >> n;
+	v1 = fizzBuzz(n);
+	for (size_t i = 0; i < v1.size(); ++i)
+		cout << v1[i] << endl;
+	//cout << endl;
 	return 0;
 }
