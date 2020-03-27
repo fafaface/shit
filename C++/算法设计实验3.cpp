626c8034b2175ff5aa922394d0f312485a983047
fafaface <halcyonluka@foxmail.com>
2020-03-27 21:57:07 +0800

    算法设计实验3_分治法

--- C++/算法设计实验3.cpp
+++ C++/算法设计实验3.cpp
@@ -8,6 +8,11 @@ struct point {
 	double y;
 }p[10001];
 
+struct point_pair {
+	point a;
+	point b;
+}minest_pair;
+
 double dist(point p, point q) {//计算距离
 	double x = fabs(p.x - q.x);//横坐标之差
 	double y = fabs(p.y - q.y);//纵坐标之差
@@ -31,7 +36,36 @@ void bf(int n)//蛮力法
 	cout << "最近对是:" << "(" << p[minpair[0]].x << "," << p[minpair[0]].y << ") " << "(" << p[minpair[1]].x << "," << p[minpair[1]].y << ")" << endl;
 }
 
-void 
+/*分治法*/
+
+point_pair bruteforce(int n)//蛮力法
+{
+
+	double mindist = 9999999;
+	point_pair ans;
+	for (int i = 0; i < n - 1; ++i)
+		for (int j = i + 1; j < n; ++j)
+		{
+			double distance = dist(p[i], p[j]);
+			if (distance < mindist) {
+				mindist = distance;
+				ans.a = p[i]; ans.b = p[j];
+			}
+		}
+	return ans;
+}
+
+point_pair closest_div(int n) {
+	if (n <= 3) {
+		return bruteforce(n);//剩下几个点,就直接蛮力法
+	}
+	int mid = n / 2;//找到中间分割点
+	point mid_point = p[mid];
+}
+double  divide_concuer(int n) {//分治法总函数
+	qsort(p, n, sizeof(point), cmp);//按照x轴坐标排序
+	return closest_div(n);//分治法 将数组对半分递归
+}
 int main(){
 	int n;//个数
 	cout << "输入点对的个数: ";
